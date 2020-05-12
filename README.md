9968b252e25f37a250531ee0f6b54405ba47cc9c
Mustapha Omotunwase <mustapha.omotunwase@ubagroup.com>
2020-05-12 10:41:54 +0100

    base code

---
+++ README.md
@@ -0,0 +1,2 @@
+# TCSM API
+A church API that allows audio and video live stream.
---
+++ src/tcsm_services/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TcsmServicesConfig(AppConfig):
+    name = 'tcsm_services'
